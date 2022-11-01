7864bef6bd31bf41ca1773842b79ddc2a46c14b7
hoarus <hoarus@gmail.com>
2022-11-01 16:53:12 -0600

    Reset components

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,25 +1,8 @@
 import React from 'react';
-import { createUseStyles } from 'react-jss';
-
-import Alert from '../Alert/Alert';
-import CartSuccess from '../CartSuccess/CartSuccess';
-
-const useStyles = createUseStyles({
-  wrapper: {
-    padding: 20,
-  }
-});
+import './App.css';
 
 function App() {
-  const classes = useStyles()
-  return(
-    <div className={classes.wrapper}>
-      <Alert title="Items Not Added" type="error">
-        <div>Your items are out of stock.</div>
-      </Alert>
-      <CartSuccess />
-    </div>
-  )
+  return <></>;
 }
 
 export default App;
