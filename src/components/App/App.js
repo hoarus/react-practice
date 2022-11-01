0362341861b1793ee262ea0f56e752e197afb68f
hoarus <hoarus@gmail.com>
2022-11-01 16:27:26 -0600

    Style using css and style objects

---
+++ src/components/App/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './App.css';
+
+import Alert from '../Alert/Alert';
+import CartSuccess from '../CartSuccess/CartSuccess';
+
+function App() {
+  const wrapper = {
+    padding: 20
+  };
+
+  
+  return (
+    <div style={wrapper}>
+      <Alert title="Items Not Added" type="error">
+        <div>Your items are out of stock.</div>
+      </Alert>
+      <CartSuccess />
+    </div>
+  )
+}
+
+export default App;
