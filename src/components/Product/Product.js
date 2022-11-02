29ded04f8f714a796108278d7c57203324478592
hoarus <hoarus@gmail.com>
2022-11-01 20:19:50 -0600

    Reset components

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,81 +1,16 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './Product.css';
 
-const products = [
-  {
-    emoji: '🍦',
-    name: 'ice cream',
-    price: 5
-  },
-  {
-    emoji: '🍩',
-    name: 'donuts',
-    price: 2.5,
-  },
-  {
-    emoji: '🍉',
-    name: 'watermelon',
-    price: 4
-  }
-];
-
-export default class Product extends Component {
-
-  state = {
-    cart: [],
-  }
-
-
-  add = (product) => {
-    this.setState(state => ({
-      cart: [...state.cart, product],
-    }))
-  }
-
-  remove = (product) => {
-    this.setState(state => {
-      const cart = [...state.cart];
-      const productIndex = cart.findIndex(p => p.name === product.name);
-      if(productIndex < 0) {
-        return;
-      }
-      cart.splice(productIndex, 1)
-      return ({
-        cart
-      })
-    })
-  }
-
-
-  currencyOptions = {
-    minimumFractionDigits: 2,
-    maximumFractionDigits: 2,
-  }
-
-  getTotal = () => {
-    const total = this.state.cart.reduce((totalCost, item) => totalCost + item.price, 0);
-    return total.toLocaleString(undefined, this.currencyOptions)
-  }
-
-  render() {
-    return(
-      <div className="wrapper">
-        <div>
-          Shopping Cart: {this.state.cart.length} total items.
-        </div>
-        <div>Total {this.getTotal()}</div>
-        <div>
-          {products.map(product => (
-            <div key={product.name}>
-              <div className="product">
-                <span role="img" aria-label={product.name}>{product.emoji}</span>
-              </div>
-              <button onClick={()=> this.add(product)}>Add</button>
-              <button onClick={()=>this.remove(product)}>Remove</button>
-            </div>
-          ))}
-        </div>
+export default function Product() {
+  return(
+    <div className="wrapper">
+      <div>
+        Shopping Cart: 0 total items.
       </div>
-    )
-  }
+      <div>Total: 0</div>
+
+      <div className="product"><span role="img" aria-label="ice cream">🍦</span></div>
+      <button>Add</button> <button>Remove</button>
+    </div>
+  )
 }
