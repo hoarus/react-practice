c5828b232c92b78896766b1e8753c5cdd4596103
hoarus <hoarus@gmail.com>
2022-11-01 20:37:36 -0600

    Implement hooks with state

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,16 +1,66 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Product.css';
 
+const currencyOptions = {
+  minimumFractionDigits: 2,
+  maximumFractionDigits: 2,
+}
+
+function getTotal(total) {
+  return total.toLocaleString(undefined, currencyOptions)
+}
+
+
+const products = [
+  {
+    emoji: '🍦',
+    name: 'ice cream',
+    price: 5
+  },
+  {
+    emoji: '🍩',
+    name: 'donuts',
+    price: 2.5,
+  },
+  {
+    emoji: '🍉',
+    name: 'watermelon',
+    price: 4
+  }
+];
+
+
 export default function Product() {
+  const [cart, setCart ] = useState([]);
+  const [total, setTotal] = useState(0);
+  
+  function add(product) {
+    setCart(current => [...current, product.name]);
+    setTotal(current => current + product.price);
+  }
+  function remove(){
+    setCart([]);
+    setTotal(0);
+  }
+
+
   return(
     <div className="wrapper">
       <div>
-        Shopping Cart: 0 total items.
+        Shopping Cart: { cart.length } total items.
       </div>
-      <div>Total: 0</div>
-
-      <div className="product"><span role="img" aria-label="ice cream">🍦</span></div>
-      <button>Add</button> <button>Remove</button>
-    </div>
+      <div>Total: {getTotal(total)}</div>
+      <div>
+        {products.map(product => (
+          <div key={product.name}>
+            <div className="product">
+              <span role="img" aria-label={product.name}>{product.emoji}</span>
+            </div>
+            <button onClick={()=> add(product)}>Add</button>
+            <button>Remove</button>
+          </div>
+        ))}
+      </div>
+  </div>
   )
 }
