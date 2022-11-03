145eafd5613796aeeea73a631fd077bd072e5191
hoarus <hoarus@gmail.com>
2022-11-02 20:32:08 -0600

    Implement shared context across components

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import Navigation from '../Navigation/Navigation';
 import SaladMaker from '../SaladMaker/SaladMaker';
+import UserContext from '../User/User';
+
+const user = {
+  name: 'Kwame',
+  favorites: [
+    'avocado',
+    'carrot'
+  ]
+}
+
 
 function App() {
   return (
-    <>
+    <UserContext.Provider value={user}>
       <Navigation />
       <SaladMaker />
-    </>
-  );
+    </UserContext.Provider>
+    );
 }
 
 export default App;
