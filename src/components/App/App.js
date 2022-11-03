15f76a4a6e0022ca2ceadf853c70adf3ce7c9edd
hoarus <hoarus@gmail.com>
2022-11-02 20:37:17 -0600

    Reset components

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,23 +1,8 @@
 import React from 'react';
-import Navigation from '../Navigation/Navigation';
-import SaladMaker from '../SaladMaker/SaladMaker';
-import UserContext from '../User/User';
-
-const user = {
-  name: 'Kwame',
-  favorites: [
-    'avocado',
-    'carrot'
-  ]
-}
-
 
 function App() {
   return (
-    <UserContext.Provider value={user}>
-      <Navigation />
-      <SaladMaker />
-    </UserContext.Provider>
+    <></>
     );
 }
 
